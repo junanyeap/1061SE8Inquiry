252220a67b0eccfcfcc4536ecdb243512f6d64ce
LAPTOP-44S3NG6I\kan <s104213059@mail1.ncnu.edu.tw>
2018-01-02 11:39:39 +0800

    search.php added and some changed

--- Views/header.php
+++ Views/header.php
@@ -3,33 +3,24 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" />
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    
+  
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default" style="background-color:#7f9c5c;">
   <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="font-size:25px;">
+      <ul class="nav navbar-nav" >
         <!-- <li class="active"><a href="#"> <span class="sr-only">(current)</span></a></li> -->
-        <li><a href="../Views/index.php">首頁</a></li>
-        <li><a href="../Views/userinfo.php">使用者清單</a></li>
-        <li><a href="../Views/SearchView.php">查看資料庫</a></li>
-        <li><a href="../Views/SearchUI.php">查詢清單</a></li>
+        <li><a href="../Views/index.php" style="color:white;font-weight:bold">首頁</a></li>
+        <li><a href="../Views/userinfo.php" style="color:white;font-weight:bold">使用者清單</a></li>
+        <li><a href="../Views/SearchView.php" style="color:white;font-weight:bold">查看資料庫</a></li>
+        <li><a href="../Views/SearchUI.php" style="color:white;font-weight:bold">查詢清單</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href='loginForm.php'>
+        <li><a href='loginForm.php' style="color:white;font-weight:bold">
           <?php 
           session_start();
           if (isset($_SESSION['uID']) && $_SESSION['uID'] == true) {
--- Views/search.php
+++ Views/search.php
@@ -4,32 +4,34 @@
     header("Location: ../Views/loginForm.php");
     exit(0);
   }
+  require("../Modules/Function.php");
 ?>
 
 <head>
   <meta charset="UTF-8" />
   <title>查詢</title>
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css">
+  <!-- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
     <!-- Custom styles for this template -->
     <link href="css/simple-sidebar.css" rel="stylesheet">
     <style>
     .card-inline {
           display:inline-block!important;
-          padding: 2px;
+          padding: 5px;
         }
   </style>
 </head>
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <div class="sidebar-navbar-fixed affix" >
-          <div class="well">
+<body >
+  <!-- <div class="container col-md-12"> -->
+    <div class="row ">
+      <div class="col-md-4">
+        <div class="sidebar-navbar-fixed affix" style="margin-left: 1.5%">
+          <div class="well" style="background-color:#b8d199;font-weight:bold" >
             <link rel = "stylesheet" type = "text/css" href = "hk.css">
-            <form action="SearchResult.php" method="post">
+            <form action="search.php" method="post" style="font-size: 25px">
             <!-- <input type="hidden" name ="act" value ="searchEcology"> -->
-            <table class="table">
-              <tr><th style="text-align:center", colspan="4">Search UI</th></tr>
+            <table class="table" >
+              <tr><th style="text-align:center", colspan="4">查詢</th></tr>
               
               <tr>
               <td style="text-align:center">關鍵字</td>
@@ -52,44 +54,33 @@
               <td style="text-align:center">屬</td>
               <td style="text-align:left"><input type="text" name="genus" placeholder="ex.屬"/></td>
               </tr>
-              <tr><th style="text-align:center", colspan="4"><input type="submit" value="Ok" /></tr>
+              <tr><th style="text-align:center", colspan="4"><input class="btn-lg" type="submit" value="確認" /></tr>
             
             </table>
           </div>
         </div>
       </div>
-      <div class="col-md-9">
-        <div class="card mb-3 card-inline">
-          <img class="card-img-top" src="http://placehold.it/200x200">
-            <div class="card-footer text-muted" type="button" onclick="window.location='login.html'">
-                名稱
-            </div>
-        </div>
-        <div class="card mb-3 card-inline">
-          <img class="card-img-top" src="http://placehold.it/200x200">
-            <div class="card-footer text-muted" type="button" onclick="window.location='login.html'">
-                名稱
-            </div>
-        </div>
-        <div class="card mb-3 card-inline">
-          <img class="card-img-top" src="http://placehold.it/200x200">
-            <div class="card-footer text-muted" type="button" onclick="window.location='login.html'">
-                名稱
-            </div>
-        </div>
-        <div class="card mb-3 card-inline">
-          <img class="card-img-top" src="http://placehold.it/200x200">
-            <div class="card-footer text-muted" type="button" onclick="window.location='login.html'">
-                名稱
-            </div>
-        </div>
+      <div class="col-md-8">
+        <?php
+        
+        $keyword = $_REQUEST['keyword'];
+        $label = $_REQUEST['label'];
+        $family = $_REQUEST['family'];
+        $genus = $_REQUEST['genus'];
+        $results=searchEcology($keyword,$label,$family,$genus);
+        while($rs=mysqli_fetch_array($results)) {
+          echo "<div class='card card-inline'>
+                  <img class='card-img-top' src='http://placehold.it/300x300'>
+                  <div class='card-footer text-center' type='button' style='font-size:25px;font-weight:bold;background-color:#b8d199'>"
+                    ,$rs['organismname'],"
+                  </div>
+                </div>";
+          
+        }
+        ?>
       </div>
     </div>
     
 
-  </div>
+  <!-- </ div> -->
 </body>
-
-<?php
-  include 'footer.php';
-?>
