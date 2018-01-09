6b3d406a4517939c34ee883b25e311e622614ba1
SkyYu93 <34836511+SkyYu93@users.noreply.github.com>
2018-01-09 16:26:05 +0800

    gototopbutton

--- Views/search.php
+++ Views/search.php
@@ -30,6 +30,7 @@
        });
     });
   </style>
+  <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>
 </head>
 <body >
   <!-- <div class="container col-md-12"> -->
@@ -122,5 +123,40 @@
         </div>
       </div>
     </div>
-
+<script>
+//<![CDATA[
+(function () {
+$("body").append("<img id='goTopButton' style='display: none; z-index: 5; cursor: pointer;' title='回到頂端' height='50' width='50'/>");
+var img = "http://i01.pic.sogou.com/8db77379d8f47267",
+locatioin = 9/10, // 按鈕出現在螢幕的高度
+right = 10, // 距離右邊 px 值
+opacity = 0.3, // 透明度
+speed = 500, // 捲動速度
+$button = $("#goTopButton"),
+$body = $(document),
+$win = $(window);
+$button.attr("src", img);
+$button.on({
+mouseover: function() {$button.css("opacity", 1);},
+mouseout: function() {$button.css("opacity", opacity);},
+click: function() {$("html, body").animate({scrollTop: 0}, speed);}
+});
+window.goTopMove = function () {
+var scrollH = $body.scrollTop(),
+winH = $win.height(),
+css = {"top": winH * locatioin + "px", "position": "fixed", "right": right, "opacity": opacity};
+if(scrollH > 20) {
+$button.css(css);
+$button.fadeIn("slow");
+} else {
+$button.fadeOut("slow");
+}
+};
+$win.on({
+scroll: function() {goTopMove();},
+resize: function() {goTopMove();}
+});
+} )();
+//]]>
+</script>
 </body>
