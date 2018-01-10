2d534f34fa4258f010bba1b130374ff0c51c3866
Unknown <junanyeap@gmail.com>
2018-01-10 10:00:47 +0800

    upload with selection

--- Views/new_upload_example.php
+++ Views/new_upload_example.php
@@ -6,7 +6,21 @@
 </head>
 <body>
     <form action="../Control/photoControl.php" method="post" enctype="multipart/form-data">
-        <input type="text" name="directory">
+        <!-- <input type="text" name="directory"> -->
+        <select id="select1" name="directory">
+          <?php
+            require("../Modules/Function.php");
+            $result = getFullList();
+            if($result->num_rows === 0) {
+              echo "Empty";
+            } else {
+              // 逐列進行動作(顯示)
+              while($row = mysqli_fetch_array($result)) {
+                echo "<option value=\"".$row['organismname']."\">".$row['organismname']."</option>";
+              }
+            }
+          ?>
+        </select>
         <!-- 使用 html 5 實現單一上傳框可多選檔案方式，須新增 multiple 元素 -->
         <input type="file" name="myFile[]" id="" accept="image/jpeg,image/jpg,image/gif,image/png" multiple>
 
